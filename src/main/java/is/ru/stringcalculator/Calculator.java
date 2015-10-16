52430af567d79ad72e16e62b5f5b74d4f379d46a
Bjarki Kristinsson <bjarkik12@ru.is>
2015-10-16 23:12:55 +0000

    Kata 4

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,15 +2,24 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
-    private static String delim = ",|\n";
+    private static final String defaultdelim = ",|\n";
 
     public static int add(String text){
+
 	if(text.equals("")){
 	    return 0;
 	}
 
+	else if( text.indexOf("//") == 0 ){
+	    String delim = "";
+	    String[] substring = text.split("\n");
+	    String[] temp = substring[0].split("//");
+	    delim = temp[1];
+	    return sum(splitNumbers(substring[1],delim));
+	}
+
 	else if(text.contains(",")){
-	    return sum(splitNumbers(text));
+	    return sum(splitNumbers(text,defaultdelim));
 	}
 
 	else{
@@ -19,12 +28,12 @@ public class Calculator {
     }
 
     private static int toInt(String number){
-		return Integer.parseInt(number);
-	}
+	return Integer.parseInt(number);
+    }
 
-    private static String[] splitNumbers(String numbers){
-	    return numbers.split(delim);
-	}
+    private static String[] splitNumbers(String numbers, String delim){
+	return numbers.split(delim);
+    }
       
     private static int sum(String[] numbers){
 	int total = 0;
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -8,17 +8,17 @@ public class CalculatorTest {
     public static void main(String args[]) {
 	org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
-
+    
     @Test
     public void testEmptyString() {
 	assertEquals(0, Calculator.add(""));
     }
-
+    
     @Test
     public void testOneNumber() {
 	assertEquals(1, Calculator.add("1"));
     }
-
+    
     @Test
     public void testTwoNumbers() {
 	assertEquals(3, Calculator.add("1,2"));
@@ -33,10 +33,16 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
 	assertEquals(55, Calculator.add("1,2,3,4,5,6,7,8,9,10"));
     }
-
+    
     @Test
     public void testNewlineDelimiter(){
 	assertEquals(10, Calculator.add("1\n2,3\n4"));
     }
+    
+    @Test
+    public void testNewDelimiter(){
+	assertEquals(3, Calculator.add("//;\n1;2"));
+    }
+    
 
 }
