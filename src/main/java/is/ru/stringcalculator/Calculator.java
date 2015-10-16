9b53bc69f5467a9655954b1f119fb168f7bc70de
Bjarki Kristinsson <bjarkik12@ru.is>
2015-10-16 21:11:14 +0000

    Kata 1

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,22 +2,24 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
-	public static int add(String text){
-		if(text.equals("")){
-			return 0;
-		}
-		else if(text.contains(",")){
-			return sum(splitNumbers(text));
-		}
-		else
-			return 1;
+    public static int add(String text){
+	if(text.equals("")){
+	    return 0;
 	}
 
-	private static int toInt(String number){
+	else if(text.contains(",")){
+	    return sum(splitNumbers(text));
+	}
+
+	else
+	    return 1;
+	}
+
+    private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
+    private static String[] splitNumbers(String numbers){
 	    return numbers.split(",");
 	}
       
@@ -28,7 +30,5 @@ public class Calculator {
 		}
 		return total;
     }
-
-
 
 }
