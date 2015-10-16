371358e7e221cbec583e5b2362125945cbddf08e
Bjarki Kristinsson <bjarkik12@ru.is>
2015-10-16 22:15:31 +0000

    Kata 3

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,6 +2,8 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
+    private static String delim = ",|\n";
+
     public static int add(String text){
 	if(text.equals("")){
 	    return 0;
@@ -11,24 +13,25 @@ public class Calculator {
 	    return sum(splitNumbers(text));
 	}
 
-	else
-	    return 1;
+	else{
+	    return toInt(text);
 	}
+    }
 
     private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
 
     private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	    return numbers.split(delim);
 	}
       
     private static int sum(String[] numbers){
- 	    int total = 0;
+	int total = 0;
         for(String number : numbers){
-		    total += toInt(number);
-		}
-		return total;
+	    total += toInt(number);
+	}
+	return total;
     }
 
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -34,4 +34,9 @@ public class CalculatorTest {
 	assertEquals(55, Calculator.add("1,2,3,4,5,6,7,8,9,10"));
     }
 
+    @Test
+    public void testNewlineDelimiter(){
+	assertEquals(10, Calculator.add("1\n2,3\n4"));
+    }
+
 }
