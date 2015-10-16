3fbe9aa3efb67413d1cd15fef5603062d6015faa
Bjarki Kristinsson <bjarkik12@ru.is>
2015-10-16 21:16:33 +0000

    Kata 2

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -5,28 +5,33 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+    public static void main(String args[]) {
+	org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
-	@Test
-	public void testEmptyString() {
-		assertEquals(0, Calculator.add(""));
-	}
+    @Test
+    public void testEmptyString() {
+	assertEquals(0, Calculator.add(""));
+    }
 
-	@Test
-	public void testOneNumber() {
-		assertEquals(1, Calculator.add("1"));
-	}
+    @Test
+    public void testOneNumber() {
+	assertEquals(1, Calculator.add("1"));
+    }
 
-	@Test
-	public void testTwoNumbers() {
-		assertEquals(3, Calculator.add("1,2"));
-	}	
+    @Test
+    public void testTwoNumbers() {
+	assertEquals(3, Calculator.add("1,2"));
+    }	
 
-	@Test
+    @Test
+    public void testThreeNumbers(){
+	assertEquals(6, Calculator.add("1,2,3"));
+    }
+
+    @Test
     public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
+	assertEquals(55, Calculator.add("1,2,3,4,5,6,7,8,9,10"));
     }
 
 }
